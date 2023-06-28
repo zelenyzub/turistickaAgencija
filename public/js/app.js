7570ebbce238cbec68f12ddb48befebf4f0d8b47
zelenyzub <savonovakovic8@gmail.com>
2023-06-28 15:21:28 +0200

    footer izmena

--- public/js/app.js
+++ public/js/app.js
@@ -14001,7 +14001,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\nfooter {\r\n    position: fixed;\r\n    bottom: 0;\r\n    width: 100%;\n}\n.mojNaslov {\r\n    text-align: center;\n}\r\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\nfooter {\r\n    position: relative;\r\n    bottom: 0;\r\n    width: 100%;\n}\n.mojNaslov {\r\n    text-align: center;\n}\r\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
--- resources/js/components/footerAdmin.vue
+++ resources/js/components/footerAdmin.vue
@@ -10,7 +10,7 @@
 </template>
 <style>
 footer {
-    position: fixed;
+    position: relative;
     bottom: 0;
     width: 100%;
 }
